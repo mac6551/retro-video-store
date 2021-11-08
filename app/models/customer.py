b7b36be90122abdccc897970fe93a90854fe5c9d
Mac <mackenziespeyton@gmail.com>
2021-11-08 11:46:44 -0800

    Adds customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,20 @@
 from app import db
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    postal_code = db.Column(db.Integer(5))
+    phone = db.Column(db.String(13))
+    register_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        """Converts model info into a dictionary"""
+        if self.id:
+            customer_dict = {
+                "id": self.id,
+                "name": self.name,
+                "postal_code": self.postal_code,
+                "phone": self.phone, 
+                "register_at": self.register_at
+            }
+        return customer_dict

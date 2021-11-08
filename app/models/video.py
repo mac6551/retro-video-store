592a32ed5401edd02506ce833cbab21f4992a634
Mac <mackenziespeyton@gmail.com>
2021-11-08 11:50:42 -0800

    Adds video model

--- app/models/video.py
+++ app/models/video.py
@@ -1,4 +1,18 @@
 from app import db
 
 class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.DateTime)
+    total_inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        """Converts model info into a dictionary"""
+        if self.id:
+            video_dict = {
+                "id": self.id,
+                "title": self.title,
+                "release_date": self.release_date,
+                "total_inventory": self.total_inventory 
+            }
+        return video_dict

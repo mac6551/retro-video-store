3bcaef002647e055c43e9278904753380db80b7f
Mac <mackenziespeyton@gmail.com>
2021-11-08 15:28:31 -0800

    edits get_customers and get_one_customer to output dictionary data and accurate 404 error codes

--- app/routes.py
+++ app/routes.py
@@ -31,8 +31,10 @@ def get_customers():
 @customer_bp.route("/<id>", methods = ["GET"])
 def get_one_customer(id):
     customer = valid_id(Customer, id)
-
+    
     if not customer: 
-        return {"message": f"Customer {id} not found"}, 404
+        return {"message": f"Customer {id} was not found"}, 404
+
+    customer = customer.to_dict()
 
     return customer, 200
